bc6d41bc2eaf04901d69f384e7ca96e686b6bc28
Sharon L <sharito.gineth19@gmail.com>
2025-11-28 08:08:29 -0500

    Modulo de linea 199 - 254

--- Proyecto_GP4/app/models.py
+++ Proyecto_GP4/app/models.py
@@ -1,3 +1,255 @@
-from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---menu, plato y cateoria-----
+
+class Categoria(models.Model):
+
+    ESTADO = [
+        ("activo", "Activo"),
+        ("inactivo", "Inactivo"),
+    ]
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    estado = models.CharField(
+        max_length=10,
+        choices=ESTADO,
+        default="activo"
+    )
+    fecha_creacion = models.DateTimeField(default=datetime.now)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        db_table = "categoria"
+
+                        
+class Plato(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Plato"
+        verbose_name_plural = "Platos"
+        db_table = "plato"
+
+class Menu(models.Model):
+    plato = models.ForeignKey(Plato, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Menu"
+        verbose_name_plural = "Menus"
+        db_table = "menu"
+
