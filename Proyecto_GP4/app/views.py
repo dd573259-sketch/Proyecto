ee888a96a678245e09bcc59141ac766be1be0df0
Daniel-Diaz <daniels_diazm@soy.sena.edu.co>
2026-02-03 06:49:34 -0500

    Implementacion de la tabla

--- Proyecto_GP4/app/views.py
+++ Proyecto_GP4/app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from app.models import Categoria
 
 def index(request):
     return render(request, 'main.html')
 # Create your views here.
+def listar_categorias(request):
+    nombre = {
+        'titulo': 'Listado de Categorias',
+        'categorias': Categoria.objects.all()
+    }
+    return render(request, 'Categoria/listar.html', nombre)
