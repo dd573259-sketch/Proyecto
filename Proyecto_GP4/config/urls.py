208dfaacfbe5f1b550b63241d52a213a1a03c9ac
0migue7 <castanedamigue0@gmail.com>
2026-06-02 08:05:30 -0500

    correcciones

--- Proyecto_GP4/config/urls.py
+++ Proyecto_GP4/config/urls.py
@@ -14,25 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from  app import views
-#from app.views.IA import urls as urls
+from app import views
 
-
-
+# from app.views.IA import urls as urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('principal/', include(('app.urls', 'app'), namespace='app')),
-    path('', include(('Login.urls', 'Login'), namespace='login')),
-    path('usuarios/', include('usuarios.urls')),
-    path('principal/', include(('usuarios.urls', 'usuarios'), namespace='usuarios')), #esta es nueva estamos experimentando
-    
+    path("admin/", admin.site.urls),
+    path("principal/", include(("app.urls", "app"), namespace="app")),
+    path("", include(("Login.urls", "Login"), namespace="login")),
+    path("usuarios/", include("usuarios.urls")),
+    # path('principal/', include(('usuarios.urls', 'usuarios'), namespace='usuarios')), #esta es nueva estamos experimentando
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
