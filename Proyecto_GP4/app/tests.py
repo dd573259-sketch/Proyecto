c313fb51b396c75264d2fd3f83375b3d08d02236
Daniel-Diaz <daniels_diazm@soy.sena.edu.co>
2026-07-03 16:51:49 -0500

    PD- Prueba3

--- Proyecto_GP4/app/tests.py
+++ Proyecto_GP4/app/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
-
+class BasicTestCase(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertIn(response.status_code, [200, 302])  # 302 si redirige a login
 
