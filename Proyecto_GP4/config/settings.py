9d56865f4a4f80f3ef855f6c867e63dca23d6b41
Daniel-Diaz <daniels_diazm@soy.sena.edu.co>
2026-07-03 16:40:03 -0500

    PD- Prueba1

--- Proyecto_GP4/config/settings.py
+++ Proyecto_GP4/config/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'la_tuna',
-        'USER': 'root',
-        'PASSWORD': '123456789', 
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
